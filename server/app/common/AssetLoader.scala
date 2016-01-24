40234157f63b5607bd7aa998f811bf7782e4b487
rich-nguyen <richnoin@gmail.com>
2016-01-24 21:12:44 +0000

    Add basic asset loader

---
+++ server/app/common/AssetLoader.scala
@@ -0,0 +1,29 @@
+package common
+
+import play.api.{Play, Mode}
+
+object AssetLoader {
+
+  private case class AssetPath(
+    path: String,
+    devPath: String,
+    prodPath: String)
+
+  private val assets = List(
+    AssetPath(
+      "app.js",
+      "http://localhost:9001/src/app.js",
+      "https://s3-eu-west-1.amazonaws.com/eightykilometres/deploys/assets/app.js")
+  )
+
+  def getPath(path: String): Option[String] = {
+    val maybePath = assets.find(_.path == path)
+    maybePath.map { asset =>
+      if (Play.current.mode == Mode.Prod) {
+        asset.prodPath
+      } else {
+        asset.devPath
+      }
+    }
+  }
+}
